14d4bff43a52139be799452f9d7a803dd64f0c6f
stephenhky <cwin31>
2016-02-15 13:23:22 -0500

    give up using scan to do gradient descent

--- ferromagnet.py
+++ ferromagnet.py
@@ -13,17 +13,17 @@ def ferromagnet_free_energy(r, u, H):
     free_energy_grad = theano.function(inputs=[m], outputs=grad_m)
 
     # minimization
-    nsteps = T.iscalar('nsteps')
-    init_m = T.dscalar('init_m')
-    m_seq = T.dscalar('m_seq')
-    grad_results, grad_updates = theano.scan(fn=lambda vars: vars-0.1*grad_m,
-                                             outputs_info=T.ones_like(m_seq),
-                                             sequences=m_seq,
-                                             n_steps=nsteps)
-    final_m = grad_results[-1]
-    cal_m = theano.function(inputs=[init_m, nsteps], outputs=final_m, updates=grad_updates)
-    # cal_m = []
+    # nsteps = T.iscalar('nsteps')
+    # init_m = T.dscalar('init_m')
+    # m_seq = T.dscalars('m_seq')
+    # grad_results, grad_updates = theano.scan(fn=lambda vars: vars - 0.1*grad_m,
+    #                                          outputs_info=[{'initial': init_m}],
+    #                                          sequences=m_seq,
+    #                                          n_steps=nsteps)
+    # final_m = grad_results[-1]
+    # cal_m = theano.function(inputs=[init_m, nsteps], outputs=final_m, updates=grad_updates)
 
-    return free_energy, [m], free_energy_grad, cal_m
+    # return free_energy, [m], free_energy_grad, cal_m
+    return free_energy, [m], free_energy_grad
 
 
