be5909fecca68d129aa0ef3cb4b8db322332d9e6
stephenhky <cwin31>
2016-02-14 22:14:51 -0500

    improving bug

--- ferromagnet.py
+++ ferromagnet.py
@@ -2,28 +2,28 @@ import numpy as np
 import theano
 import theano.tensor as T
 
-def ferromagnet_free_energy():
-    m = T.iscalar('m')
-    r = T.iscalar('r')
-    u = T.iscalar('u')
-    H = T.iscalar('H')
-    variables = [m]
-    params = [r, u, H]
+def ferromagnet_free_energy(r, u, H):
+    # variables
+    m = T.dscalar('m')
 
+    # free energy and its gradient
     F = 0.5*r*m*m+0.25*u*m*m*m*m-H*m
-    free_energy_gradients = [T.grad(F, m)]
+    grad_m = T.grad(F, m)
+    free_energy = theano.function(inputs=[m], outputs=F)
+    free_energy_grad = theano.function(inputs=[m], outputs=grad_m)
 
-    free_energy = theano.function(inputs=[r, u, H, m], outputs=F)
-
-    return free_energy, variables, free_energy_gradients, params
-
-def minimize_free_energy(variables, grad_variables):
+    # minimization
     nsteps = T.iscalar('nsteps')
-    init_variables = T.iscalar('init_variables')
-    grad_results, grad_updates = theano.scan(fn=lambda vars, gvars: vars-0.1*gvars,
-                                             outputs_info=[{'initial': init_variables}],
-                                             sequences=variables,
-                                             non_sequences=grad_variables,
+    init_m = T.dscalar('init_m')
+    m_seq = T.dscalar('m_seq')
+    grad_results, grad_updates = theano.scan(fn=lambda vars: vars-0.1*grad_m,
+                                             outputs_info=T.ones_like(m_seq),
+                                             sequences=m_seq,
                                              n_steps=nsteps)
-    cal_newparam = theano.function(inputs=[variables, nsteps], outputs=grad_results, updates=grad_updates)
-    return cal_newparam
+    final_m = grad_results[-1]
+    cal_m = theano.function(inputs=[init_m, nsteps], outputs=final_m, updates=grad_updates)
+    # cal_m = []
+
+    return free_energy, [m], free_energy_grad, cal_m
+
+
